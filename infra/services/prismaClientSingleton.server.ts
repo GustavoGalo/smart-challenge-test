1eb75380abf592a1e2004dfb8725615120a27230
Gustavo Galo <gg.gustavo.galo@gmail.com>
2024-01-03 01:40:25 -0300

    creating a singleton for prisma client

---
+++ infra/services/prismaClientSingleton.server.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+
+export class PrismaClientSingleton {
+  private static instance: PrismaClientSingleton;
+  private prisma = new PrismaClient();
+
+  private constructor() {}
+
+  public static getInstance() {
+    if (!PrismaClientSingleton.instance)
+      PrismaClientSingleton.instance = new PrismaClientSingleton();
+    return PrismaClientSingleton.instance;
+  }
+
+  public getPrismaClient() {
+    return this.prisma;
+  }
+
+  public async connect() {
+    await this.prisma.$connect();
+  }
+
+  public async disconnect() {
+    await this.prisma.$disconnect();
+  }
+}
