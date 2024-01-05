93076e242ed3057583f8fb3c20b42218952864af
Gustavo Galo <gg.gustavo.galo@gmail.com>
2024-01-05 05:55:03 -0300

    feat: add filters param in addProducts use case

--- app/usecases/listProducts.server.ts
+++ app/usecases/listProducts.server.ts
@@ -1,9 +1,41 @@
+import { Filter } from "../interfaces/entities/filter";
 import { ProductRepository, PrismaClientSingleton } from "../../infra/";
 
-export async function listProducts() {
+export async function listProducts(filters?: Filter) {
+  console.log(filters);
   const prismaClient = PrismaClientSingleton.getInstance().getPrismaClient();
   const productsRepository = new ProductRepository(prismaClient);
-  const products = await productsRepository.listAll();
 
-  return products;
+  const filterObj = {};
+  const page = filters?.page || 1;
+  const totalItemsInPage = 6;
+  const totalItemsToSkip = (page - 1) * totalItemsInPage;
+
+  if (filters && filters.orderBy)
+    Object.assign(filterObj, {
+      orderBy: {
+        price: filters.orderBy,
+      },
+    });
+  if (filters && filters.search)
+    Object.assign(filterObj, {
+      where: {
+        name: {
+          contains: filters.search,
+          mode: "insensitive",
+        },
+      },
+    });
+
+  const productRecords = await productsRepository.countRecords(filterObj);
+  const pages = Math.ceil(productRecords / totalItemsInPage);
+
+  Object.assign(filterObj, {
+    take: totalItemsInPage,
+    skip: totalItemsToSkip,
+  });
+
+  const products = await productsRepository.listAllWithFilters(filterObj);
+
+  return { data: products, page, pages };
 }
