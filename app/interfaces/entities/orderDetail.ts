8d1df7737b772b050163793efbba6fd4858cd2d5
Gustavo Galo <gg.gustavo.galo@gmail.com>
2024-01-05 09:51:15 -0300

    fix: corresponding new order details interface

--- app/interfaces/entities/orderDetail.ts
+++ app/interfaces/entities/orderDetail.ts
@@ -1,9 +1,14 @@
+import { Order } from "./order";
+import { Product } from "./product";
+
 export interface OrderDetail {
   id: number;
   orderId: number;
   productId: number;
   count: number;
   unitPrice: number;
+  order?: Order;
+  product?: Product;
 }
 
 export interface OrderDetailDTO {
